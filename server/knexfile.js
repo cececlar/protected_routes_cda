b25849b3181fa07c565b1b9215fbe81271480d27
Will <wkennedy210@gmail.com>
2021-04-14 10:16:08 -0400

    knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
-    user: 'root',
-    password: '6560256wK1!',
-    database: 'todoapp',
+    user: '<YOUR DB USER>',
+    password: '<YOUR DB PASSWORD>',
+    database: '<YOUR DB NAME>',
     charset: 'utf8'
   }
 };
