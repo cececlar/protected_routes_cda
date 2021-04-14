31a60edd16a5cf7ceaf4454253dfac443152c116
Will <wkennedy210@gmail.com>
2021-04-13 23:39:53 -0400

    add cors

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/users');
-const taskRoutes = require('./routes/tasks');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-// app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
