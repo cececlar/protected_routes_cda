540ff3c8d385e5b1a83c201d8d84e44363d637d0
cececlar <cece@wyncode.co>
2021-04-19 16:18:05 -0400

    update App.js with Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import './App.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
   );
