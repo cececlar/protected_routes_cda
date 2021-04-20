8749fbfc44085d62dffd960ab54585f6b986da05
cececlar <cece@wyncode.co>
2021-04-20 18:27:04 -0400

    import path module

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const userRoutes = require("./routes/users");
 const cors = require("cors");
-// const mysql = require("mysql");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
